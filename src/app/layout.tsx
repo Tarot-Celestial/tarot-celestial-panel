87f0c2911bf43709d271442fb6009312221d4e80
Tarot-Celestial <caudetricky@gmail.com>
2026-02-23 23:04:16 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Panel Interno - Tarot Celestial"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
